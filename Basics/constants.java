1d4b1f75416b9cc0e6ee66fc3a72be812ee7dc8a
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-12-28 23:13:50 +0530

    final keyword

---
+++ Basics/constants.java
@@ -0,0 +1,12 @@
+public class constants{
+	public static void main(String[] arg){
+		//constants are variable whose value cannot be changed
+		// any attempt to change(reassign) such varibles will result in error
+		
+		final double PI = 3.14159265;
+		
+		System.out.println(PI);
+		
+		//PI = 3.14;  //this line will cause error;
+	}
+}
