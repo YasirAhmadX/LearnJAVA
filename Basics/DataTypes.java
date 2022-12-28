aa4c95136166b022c78bdf55ccd789b76335f82a
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-12-28 11:33:33 +0530

    Data types

---
+++ Basics/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes{
+	public static void main(String[] args){
+		// Primitive Data Types
+		//byte  : 1   [-128 to 127]
+		//short : 2		
+		//int   : 4		
+		//long  : 8		
+		//float : 4		
+		//double: 8		
+		//char  : 2		
+		//boolean  : 1 	[true/false]
+		byte age = 30;
+		int pin = 835301;
+		long phone  = 72829656321L;
+		float pi = 3.14F;
+		char letter = '@';
+		boolean isAdult = false;
+	}
+}
---
+++ Basics/DataTypes2.java
@@ -0,0 +1,7 @@
+public class DataTypes2{
+	public static void main(String[] args){
+		//Non-primitive Data Types
+		String name = "Hello";
+		System.out.println(name.length());
+	}
+}
