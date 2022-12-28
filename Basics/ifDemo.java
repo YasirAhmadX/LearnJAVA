90f29fec4eccd9bde555e29e8a07b90145e67c6a
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-12-28 20:18:33 +0530

    if, else if, else and ternary opt

--- Basics/ifDemo.java
+++ Basics/ifDemo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ifDemo{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter you're age: ");
+		System.out.println("Enter your age: ");
 		int userAge = input.nextInt();
 		if( userAge < 0 || userAge > 100){
 			System.out.println("Invalid Age!");
@@ -12,6 +12,10 @@ public class ifDemo{
 		}
 		else{
 			System.out.println("Adult\nYou can vote!!");
+			
 		}
+		//ternary operators are also supported
+		// (condition) ? (true case) : (false case)
+		System.out.println((18<=userAge && userAge<=100) ? "adult" : "cant vote");
 	}
 }
