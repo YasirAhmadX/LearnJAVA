db8fa3642398c9acc5a0374b6acd608905575942
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2022-12-31 22:09:15 +0530

    Solid square
    4
    ****
    ****
    ****
    ****

--- Intermediate/PatternProblems/P1.java
+++ Intermediate/PatternProblems/P1.java
@@ -3,7 +3,6 @@ public class P1{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		
 		for(int i=0;i<n;i++){
 			for(int j=0;j<n;j++){
 				System.out.print("*");
