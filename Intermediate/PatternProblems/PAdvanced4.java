7a866857f50af7306eb2978eead74c02cc9ded20
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-01-01 15:48:24 +0530

    palindromic pyramid
    5
        1
       212
      32123
     4321234
    543212345

--- Intermediate/PatternProblems/PAdvanced4.java
+++ Intermediate/PatternProblems/PAdvanced4.java
@@ -8,10 +8,10 @@ public class PAdvanced4 {
                 System.out.print(" ");
             }
             for(int j=i; j>=1; j--){
-                System.out.print(j+" ");
+                System.out.print(j);
             }
             for(int j=2; j<=i; j++){
-                System.out.print(j+" ");
+                System.out.print(j);
             }
             System.out.println();
         }
